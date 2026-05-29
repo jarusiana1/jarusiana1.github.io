cd62576078af77821f45b5e00da3c43876f1e8f4
jarusiana1 <jarusiana.student@asiancollege.edu.ph>
2026-05-29 20:02:15 +0800

    Delete UPSCALE2 directory

--- UPSCALE2/attraction.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("DOMContentLoaded", function () {
-  // Animate all card images on load
-  const images = document.querySelectorAll(".card img");
-
-  images.forEach(img => {
-    img.classList.add("animate");
-  });
-});
--- UPSCALE2/officer.js
+++
@@ -1,3 +0,0 @@
-function showName(name) {
-  alert("Officer: " + name);
-}
